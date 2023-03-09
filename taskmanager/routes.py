f73c226de905e24f5d261b27236aa14fe7465e80
victormgbam <victormgbam@hotmail.com>
2023-03-09 11:23:14 +0000

    initial commit

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -60,3 +60,22 @@ def add_task():
         return redirect(url_for("home"))
     return render_template("add_task.html", categories=categories)
 
+
+@app.route("/edit_task/<int:task_id>", methods=["GET", "POST"])
+def edit_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    categories = list(Category.query.order_by(Category.Category_name).all())
+    if request.method == "POST":
+
+        task.task_name = request.form.get("task_name")
+        task.task_description = request.form.get("task_description")
+        task.is_urgent = bool(True if request.form.get("is_urgent") else False)
+        task.due_date = request.form.get("due_date") 
+        task.category_id = request.form.get("category_id")
+        category = Category(Category_name=request.form.get("Category_name"))
+        db.session.add(task)
+        db.session.commit() 
+    return render_template("edit_task.html", task=task, categories=categories)
+
+
+
